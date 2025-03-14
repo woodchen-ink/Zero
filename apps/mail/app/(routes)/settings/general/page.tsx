8c1acd8a7ba0dd76c35a6fae0b2c455790048206
user12224 <122770437+user12224@users.noreply.github.com>
2025-03-14 02:49:07 +0200

    minor

--- apps/mail/app/(routes)/settings/general/page.tsx
+++ apps/mail/app/(routes)/settings/general/page.tsx
@@ -41,6 +41,7 @@ export default function GeneralPage() {
   const router = useRouter();
   const [isSaving, setIsSaving] = useState(false);
   const locale = useLocale();
+  const t = useTranslations();
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -61,7 +62,7 @@ export default function GeneralPage() {
 
     if (values.language !== locale) {
       const localeName = new Intl.DisplayNames([values.language], { type: 'language' }).of(values.language);
-      toast.success(t("pages.settings.general.languageChangedTo", { language: localeName }))
+      toast.success(t("pages.settings.general.languageChangedTo", { language: localeName }));
     }
 
     // Simulate API call
@@ -87,8 +88,6 @@ export default function GeneralPage() {
       },
     );
   };
-
-  const t = useTranslations();
 
   return (
     <div className="grid gap-6">
