741c98aeb8ea1e1f1b26ca29a24521d0cc9f0f87
Aj Wazzan <x_1337@outlook.com>
2025-04-03 17:45:12 -0700

    saving...

---
+++ apps/mail/app/api/driver/route.ts
@@ -0,0 +1,66 @@
+import { type NextRequest, NextResponse } from 'next/server';
+import { getActiveDriver } from '@/actions/utils';
+import { Ratelimit } from '@upstash/ratelimit';
+import { defaultPageSize } from '@/lib/utils';
+import { getMails } from '@/actions/mail';
+import { redis } from '@/lib/redis';
+
+const ratelimit = new Ratelimit({
+  redis,
+  limiter: Ratelimit.slidingWindow(5000, '1m'),
+  analytics: true,
+  prefix: 'ratelimit:list-threads',
+});
+
+export const GET = async (req: NextRequest) => {
+  const ip = req.headers.get('CF-Connecting-IP');
+  if (!ip) {
+    console.log('No IP detected');
+    return NextResponse.json({ error: 'No IP detected' }, { status: 400 });
+  }
+  console.log(
+    'Request from IP:',
+    ip,
+    req.headers.get('x-forwarded-for'),
+    req.headers.get('CF-Connecting-IP'),
+  );
+  const { success, limit, reset, remaining } = await ratelimit.limit(ip);
+  const headers = {
+    'X-RateLimit-Limit': limit.toString(),
+    'X-RateLimit-Remaining': remaining.toString(),
+    'X-RateLimit-Reset': reset.toString(),
+  };
+  if (!success) {
+    console.log(`Rate limit exceeded for IP ${ip}. Remaining: ${remaining}`, body.email);
+    return NextResponse.json(
+      { error: 'Too many requests. Please try again later.' },
+      { status: 429, headers },
+    );
+  }
+  const searchParams = req.nextUrl.searchParams;
+  let [folder, pageToken, q, max] = [
+    searchParams.get('folder'),
+    searchParams.get('pageToken'),
+    searchParams.get('q'),
+    Number(searchParams.get('max')),
+  ];
+  if (!folder) folder = 'inbox';
+  if (!pageToken) pageToken = '';
+  if (!q) q = '';
+  if (!max) max = defaultPageSize;
+  const threadsResponse = await getMails({
+    folder,
+    q,
+    max,
+    pageToken,
+    labelIds: undefined,
+  });
+  return NextResponse.json(threadsResponse, {
+    status: 200,
+    headers: {
+      'X-RateLimit-Limit': limit.toString(),
+      'X-RateLimit-Remaining': remaining.toString(),
+      'X-RateLimit-Reset': reset.toString(),
+    },
+  });
+};
