3974b24b74739daa7a65ce06831aa514b2d02e8e
Ayush Kumar <ayush23162@iiitd.ac.in>
2025-04-20 19:25:26 +0530

    fix: invite dialog colors #732

--- apps/mail/components/golden.tsx
+++ apps/mail/components/golden.tsx
@@ -97,7 +97,7 @@ export const GoldenTicketModal = () => {
       <DialogContent>
         <DialogHeader>
           <DialogTitle className='flex flex-col gap-4'>
-            <Image src='/white-icon.svg' alt='Zero' width={32} height={32} />
+            <Image src='/white-icon.svg' alt='Zero' width={32} height={32} className='invert dark:invert-0'/>
             <span>Welcome to Zero! 🎉 ✨</span>
           </DialogTitle>
           <DialogDescription className='pt-3 flex flex-col gap-3'>
@@ -115,7 +115,7 @@ export const GoldenTicketModal = () => {
               render={({ field }) => (
                 <FormItem>
                   
-                  <Input placeholder='nizzy@gmail.com' {...field} className='placeholder:opacity-20' />
+                  <Input placeholder='nizzy@gmail.com' {...field} className='placeholder:opacity-60 dark:placeholder:opacity-20' />
                 </FormItem>
               )}
             />
