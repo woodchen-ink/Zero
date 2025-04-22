934e336aeaf3d193efc3b2ca1243b3b290a8cbab
Aj Wazzan <x_1337@outlook.com>
2025-04-22 13:28:13 -0700

    Update session check to include user id before redirecting

--- apps/mail/app/page.tsx
+++ apps/mail/app/page.tsx
@@ -5,7 +5,7 @@ import { redirect } from 'next/navigation';
 
 export default async function Home() {
   const session = await getSession();
-  if (session) {
+  if (session.data?.user.id) {
     redirect('/mail/inbox');
   }
 
