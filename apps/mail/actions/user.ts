e5edcbec18d790cfbeb029d35fdbb64e266dfd1b
Ahmet Kilinc <akx9@icloud.com>
2025-04-24 17:30:13 +0100

    correct way to delete accounts

--- apps/mail/actions/user.ts
+++ apps/mail/actions/user.ts
@@ -1,7 +1,10 @@
 'use server';
 
+import { connection, user } from '@zero/db/schema';
 import { headers } from 'next/headers';
 import { auth } from '@/lib/auth';
+import { eq } from 'drizzle-orm';
+import { db } from '@zero/db';
 
 export async function deleteUser() {
   const headersList = await headers();
@@ -9,17 +12,23 @@ export async function deleteUser() {
 
   if (!session) return { success: false, error: 'Session not found' };
 
-  const token = session.session.token;
   const userId = session.user?.id;
 
-  if (!token) return { success: false, error: 'Token not found' };
   if (!userId) return { success: false, error: 'User not found' };
 
   const { success, message } = await auth.api.deleteUser({
     body: {
       callbackURL: '/login',
-      token,
     },
+    headers: headersList,
   });
+
+  if (success) {
+    await db.delete(connection).where(eq(connection.userId, userId));
+    await db.delete(user).where(eq(user.id, userId));
+
+    await auth.api.signOut({ headers: headersList });
+  }
+
   return { success, message };
 }
