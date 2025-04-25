27ab0d022bc327c0729378923263845a8610d07d
Ahmet Kilinc <akx9@icloud.com>
2025-04-25 02:28:32 +0100

    icon error fix

--- apps/mail/components/icons/icons.tsx
+++ apps/mail/components/icons/icons.tsx
@@ -1010,11 +1010,18 @@ export const Suitcase = ({ className }: { className?: string }) => (
 );
 
 export const ChevronDown = ({ className }: { className?: string }) => (
-  <svg width="8" height="6" viewBox="0 0 8 6" fill="none" xmlns="http://www.w3.org/2000/svg" className={className}>
+  <svg
+    width="8"
+    height="6"
+    viewBox="0 0 8 6"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    className={className}
+  >
     <path
       id="Union"
-      fill-rule="evenodd"
-      clip-rule="evenodd"
+      fillRule="evenodd"
+      clipRule="evenodd"
       d="M0.21967 0.84467C0.512563 0.551777 0.987437 0.551777 1.28033 0.84467L4 3.56434L6.71967 0.844671C7.01256 0.551778 7.48744 0.551778 7.78033 0.844671C8.07322 1.13756 8.07322 1.61244 7.78033 1.90533L4.53033 5.15533C4.23744 5.44822 3.76256 5.44822 3.46967 5.15533L0.21967 1.90533C-0.0732233 1.61244 -0.0732233 1.13756 0.21967 0.84467Z"
     />
   </svg>
